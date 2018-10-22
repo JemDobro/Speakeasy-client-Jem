f87563bc686dd1241095544cfdf18c64d2ff66ef
Jemadean Dobrosielski <jemadean@gmail.com>
2018-10-22 16:53:32 -0700

    added Deployed to check deployment

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -12,7 +12,7 @@ export function LandingPage(props) {
 
     return (
         <div className="home">
-            <h2>Welcome to Speakeasy...do you know the secret knock?</h2>
+            <h2>Deployed  Welcome to Speakeasy...do you know the secret knock?</h2>
             <LoginForm />
             <p>Do you find yourself nodding along with your smart tech friends, coworkers, or worse, your boss, as if you understand the latest tech jargon? All the while, secretly hoping that you won't have to feign choking on your drink if someone asks you a question? Are you yearning to get into the club but can't quite speak the language? Step into Speakeasy, learn the language, up your stats and your status as you keep up to date with all the latest secret knocks that open doors... </p>
             <Link to="/register">Become one of the club</Link>
