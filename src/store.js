6079e5e3dd4b1353f2942ba3e0e55bfa5b89950c
Jemadean Dobrosielski <jemadean@gmail.com>
2018-10-23 01:30:07 -0700

    made redux-devtools-extension available to production

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ import {loadAuthToken} from './local-storage';
 import authReducer from './reducers/auth';
 import protectedDataReducer from './reducers/protected-data';
 import {setAuthToken, refreshAuthToken} from './actions/auth';
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const store = createStore(
     combineReducers({
