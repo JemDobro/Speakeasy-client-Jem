6c8663732e5f5acd6013631122eebca1d693b144
Jemadean Dobrosielski <jemadean@gmail.com>
2018-10-23 04:15:50 -0700

    added build

---
+++ build/precache-manifest.e8963117bb13795417fc1a6d7ca9260f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4b1cf334b860bfb90d3d",
+    "url": "/static/js/main.4b1cf334.chunk.js"
+  },
+  {
+    "revision": "7e7f1f18cce7b4d3c5db",
+    "url": "/static/js/1.7e7f1f18.chunk.js"
+  },
+  {
+    "revision": "4b1cf334b860bfb90d3d",
+    "url": "/static/css/main.7037f5a1.chunk.css"
+  },
+  {
+    "revision": "f3a0028462a63da2139b0552d9c8fa22",
+    "url": "/index.html"
+  }
+];
