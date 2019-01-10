782388321031a42dd2b1eb19f1920e25a6e43761
Jemadean Dobrosielski <jemadean@gmail.com>
2019-01-10 15:57:15 -0800

    added demo user to Info

--- src/components/Info.js
+++ src/components/Info.js
@@ -8,6 +8,9 @@ export function Info(props) {
             <p>You'll be presented with a question, and once you submit your answer, you will immediately receive feedback to let you know if you are indeed correct!  If not, we'll give you the correct answer so you can learn it for next time.</p> 
             <p>The more trouble you have with a question, the more you will see it, and the less trouble you have with a question, the less you will see it.  This spaced repetition system will have you speaking the lingo faster, upping your status and your stats!</p>
             <p>Speaking of stats, you will see your stats for the current session as you answer questions--keep looking for that 100%! You can reset the current session stats, see your All Time Stats, or come back to this info at any time you want...we have buttons for that.  Let's go!</p>
+            <p className="demoUser">**We recommend you create an account to start from zero questions answered and personalize the spaced repetition to you, but if you'd just like to take a test run, feel free to login with:</p>
+            <p>Username: ellafitzgerald</p>
+            <p>Password: ellafitzgerald</p>
         </section>
     );
 }
