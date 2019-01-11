6a25562e20e9d4cf1e3da94ab0131014bea266db
Jemadean Dobrosielski <jemadean@gmail.com>
2019-01-10 18:33:55 -0800

    handled reg errors

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Field, reduxForm, focus} from 'redux-form';
-import {registerUser} from '../actions/users';
+import {registerUser} from '../actions/registration';
 import {login} from '../actions/auth';
 import {clearSession} from '../actions/questions';
 import Input from './Input';
