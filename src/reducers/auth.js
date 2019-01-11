699795d91b66fcef192ba322d73f4eebc62d89f0
Jemadean Dobrosielski <jemadean@gmail.com>
2019-01-10 18:39:47 -0800

    fixed spacing on switch statements in reducers, nicer msgs on validators

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -19,18 +19,19 @@ const initialState = {
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case SET_AUTH_TOKEN:
-    return {...state, authToken: action.authToken};
+      return {...state, authToken: action.authToken};
     case CLEAR_AUTH:
-    return {...state, authToken: null, currentUser: null };
+      return {...state, authToken: null, currentUser: null };
     case AUTH_REQUEST:
-    return {...state, loggedOut: false, loading: true, error: null };
+      return {...state, loggedOut: false, loading: true, error: null };
     case AUTH_SUCCESS:
-    return {...state, loading: false, currentUser: {firstName: action.currentUser.firstName}};
+      return {...state, loading: false, currentUser: {firstName: action.currentUser.firstName}};
     case AUTH_ERROR:
-    return {...state, loading: false, error: action.error};
+      return {...state, loading: false, error: action.error};
     case TOGGLE_LOGGED_OUT:
-    return {...state,  loggedOut: !state.loggedOut};
-    default: return state;
+      return {...state,  loggedOut: !state.loggedOut};
+    default: 
+      return state;
   }
 }
 
